87f1b2ff324ba803562d0cd5a0cf1cdaa2d250e3
whitesadd <david.blancop@gmail.com>
2017-02-10 22:41:25 +0100

    UnitTest Count/1 and Draw/1
    Change-Id: I705c186b7017b2b57c848df629c6b87efc787508

--- test/deck_test.erl
+++ test/deck_test.erl
@@ -3,6 +3,28 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-init_stop_test() ->
+-define(NO_OF_CARDS, (52 * 8)).
+
+init_stop_test_() ->
+    ?_assert(ok =:= deck:stop(deck:init())).
+
+count_test_() ->
+    ?_assertEqual(?NO_OF_CARDS, deck:count(deck:init())).
+
+%% Auxiliary Functions
+
+draw_test_() ->
     Pid = deck:init(),
-    ok = deck:stop(Pid).
+    Count = deck:count(Pid),
+    ?_assertEqual(Count, length(draw_all_cards(Pid))).
+
+draw_all_cards(Pid) ->
+    draw_all_cards(Pid, []).
+
+draw_all_cards(Pid, DrawnCardList) ->
+    case deck:draw(Pid) of
+        empty ->
+            lists:reverse(DrawnCardList);
+        Card ->
+            draw_all_cards(Pid, [Card | DrawnCardList])
+    end.
