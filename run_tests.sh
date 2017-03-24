9a8ec5f86d3aa5d0e7456983b5d53765ffa93e99
Per Fryking <fryking@gmail.com>
2017-03-24 20:21:35 +0100

    Build blackjack before running tests
    Change-Id: Icc76db0f59d5602ee51decc121bffd292f527e82

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+## Abort on error
+set -e
+
 if [[ $(uname) == *"CYGWIN"* ]]; then
   EBIN=$(cygpath -w $PWD/ebin)
 else
   EBIN=$PWD/ebin
 fi
 
+erl -make
 ct_run -noshell -pa $EBIN -include ./include -dir ./test -logdir ./var/logs -cover ./cover.spec
